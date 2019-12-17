5573c2c639e5250e61c65aab1cd0c1cb096e4909
Jack Savage <jack.savage@bjss.com>
2019-12-17 14:51:54 +0000

    Refactored FizzBuzz inline

--- exercises/java/src/main/java/refactoring/FizzBuzzCreator.java
+++ exercises/java/src/main/java/refactoring/FizzBuzzCreator.java
@@ -8,43 +8,58 @@ class FizzBuzzCreator {
     public static final int FIZZ_BUZZ_LIMIT = Byte.MAX_VALUE - 27;
     public static final int THREE = 0b11;
     public static final int FIVE = new int[]{0, 0, 0, 0, 0}.length;
-    public static final String BUZZ_TEXT = new String(DatatypeConverter.parseHexBinary("42757a7a"), StandardCharsets.UTF_8);
-    public static final String FIZZ_TEXT = new String(DatatypeConverter.parseHexBinary("46697a7a"), StandardCharsets.UTF_8);
+    public static final String BUZZ_TEXT = hexToString("42757a7a");
+    public static final String FIZZ_TEXT = hexToString("46697a7a");
 
     private int fizzCountup;
     private int buzzCountdown = FIVE;
 
     String fizzBuzz() {
         String fizzBuzzBuilder = "";
-        int fizzBuzzLimit = FIZZ_BUZZ_LIMIT;
-        for (int currentDigit = 0; currentDigit < fizzBuzzLimit; currentDigit++) fizzBuzzBuilder += convertDigitToFizzBuzz(currentDigit) + " ";
-        String fizzBuzzOutput = fizzBuzzBuilder.substring(0, fizzBuzzBuilder.length() - 1);
-        return fizzBuzzOutput;
+        for (int currentDigit = 0; currentDigit < FIZZ_BUZZ_LIMIT; currentDigit++)
+            fizzBuzzBuilder += convertDigitToFizzBuzz(currentDigit) + " ";
+        return removeLastCharacter(fizzBuzzBuilder);
     }
 
-    private String convertDigitToFizzBuzz(int counterTo100) {
+    private String removeLastCharacter(String input) {
+        return input.substring(0, input.length() - 1);
+    }
+
+    private String convertDigitToFizzBuzz(int digit) {
         fizzCountup++;
         buzzCountdown--;
-        int three = THREE;
-        String numericalValue = String.valueOf(counterTo100 + 1);
-        String resultValue = fizzCountup == three || buzzCountdown == 0 ? "" : numericalValue;
-        boolean isDivisibleByThree = fizzCountup == three;
-        boolean isDivisibleByFive = buzzCountdown == 0;
-        if (isDivisibleByThree) resultValue += resetFizzCounterReturnFizz();
-        if (isDivisibleByFive) resultValue += resetBuzzCounterReturnBuzz();
+
+        String resultValue = isValueThree(fizzCountup) || isValueZero(buzzCountdown) ? "" : indexToNumericalValueAsString(digit);
+
+        if (isValueThree(fizzCountup)) resultValue += resetFizzCounterReturnFizz();
+        if (isValueZero(buzzCountdown)) resultValue += resetBuzzCounterReturnBuzz();
+
         return resultValue;
     }
 
+    private boolean isValueZero(int value) {
+        return value == 0;
+    }
+
+    private boolean isValueThree(int value) {
+        return value == THREE;
+    }
+
+    private String indexToNumericalValueAsString(int counterTo100) {
+        return String.valueOf(counterTo100 + 1);
+    }
+
     private String resetBuzzCounterReturnBuzz() {
-        int five = FIVE;
-        buzzCountdown = five;
-        String buzzText = BUZZ_TEXT;
-        return buzzText;
+        buzzCountdown = FIVE;
+        return BUZZ_TEXT;
     }
 
     private String resetFizzCounterReturnFizz() {
         fizzCountup = 0;
-        String fizzText = FIZZ_TEXT;
-        return fizzText;
+        return FIZZ_TEXT;
+    }
+
+    private static String hexToString(String hex) {
+        return new String(DatatypeConverter.parseHexBinary(hex), StandardCharsets.UTF_8);
     }
 }
